3e62dcea19fa97845cb06a7ac2143c766d4b13ce
FaresA15 <faresaneq96@gmail.com>
2025-01-09 13:51:52 +0200

    ok

--- src/main/java/HomeWork4/HomeWork4/Triangle_classification.java
+++ src/main/java/HomeWork4/HomeWork4/Triangle_classification.java
@@ -1,34 +1,40 @@
- package HomeWork4.HomeWork4;
-
+package HomeWork4.HomeWork4;
 
 public class Triangle_classification {
 
-enum Triangle_Types {
-equilateral,
-isosceles,
-Scalene,
-Nottriangle,
-ERROR
+	enum Triangle_Types {
+		equilateral, isosceles, Scalene, Nottriangle, ERROR
 
-}
+	}
+		public static String classifyTriangle(int a, int b, int c) {
+			Triangle_Types triangle;
+		        if (a + b > c) {
+		            if ( a + c > b) {
+		                if (b + c > a) {
+		                    if (a == b) {
+		                        if (b == c) {
+		                            triangle =Triangle_Types.equilateral ;
+		                        } else {
+		                            triangle = Triangle_Types.isosceles;
+		                        }
+		                    } else if (a == c) {
+		                        triangle = Triangle_Types.isosceles;
+		                    } else if (b == c) {
+		                        triangle = Triangle_Types.isosceles;
+		                    } else {
+		                        triangle = Triangle_Types.Scalene;
+		                    }
+		                } else {
+		                    triangle = Triangle_Types.Nottriangle;
+		                }
+		            } else {
+		                triangle = Triangle_Types.Nottriangle;
+		            }
+		        } else {
+		            triangle = Triangle_Types.Nottriangle;
+		        }
+		        return triangle.toString();
 
-
-
-public static String classifyTriangle(int a, int b, int c) {
-
-Triangle_Types triangle;
-if((c < a+b) && (b < a+c) && (a < b+c) )
-{
-if((a==b) && (b==c) ) 
-triangle= Triangle_Types.equilateral;
-else if ((a==b) || (b==c) || (a==c))
-triangle= Triangle_Types.isosceles;
-else
-triangle= Triangle_Types.Scalene;
-
-}else
-triangle= Triangle_Types.Nottriangle;
-
-return triangle.toString();
-}
+		
+	}
 }
